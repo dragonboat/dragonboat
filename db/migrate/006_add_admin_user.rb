3ce63565468d34dbebeae0432b3d2d839f4040ec
dina <dinchik-i@rambler.ru>
2008-10-30 16:58:04 +0200

    ome Changes

--- db/migrate/006_add_admin_user.rb
+++
@@ -1,9 +0,0 @@
-class AddAdminUser < ActiveRecord::Migration
-  def self.up
-    @user = User.create({:login=>"admin",:email => "din.chick@gmail.com", :password => "admin", :password_confirmation=> "admin" })
-  end
-
-  def self.down
-   User.destroy_all(:login=>"admin")
-  end
-end
