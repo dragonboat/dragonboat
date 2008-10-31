8f4087ef0b98513cf66d31dceb9da61953a396f2
dina <dinchik-i@rambler.ru>
2008-10-31 16:28:57 +0200

    News Update

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -6,7 +6,7 @@ class NewsController < ApplicationController
   
   def show
     @news = News.find_actual(params[:id])
-    @news_contents = News.find_actual(:all, :limit=>APP_CONFIG["news_per_page"])
+    @news_contents = News.find_actual(:all, :conditions=>"news.id <> #{params[:id]}", :limit=>APP_CONFIG["news_per_page"])
   end
 
 end
