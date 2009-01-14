186dc9f2b7ba31ab5437448b21380113d9a81864
dina <dinchik-i@rambler.ru>
2009-01-14 13:59:44 +0200

    Upload changes..

--- config/routes.rb
+++ config/routes.rb
@@ -22,8 +22,7 @@ ActionController::Routing::Routes.draw do |map|
     #m.profile '/profile/:id',    :controller => 'member/users', :action=>"show"
     m.resource   :tent, :controller => 'member/tents'
     
-    map.resources :users, :user => { :index => :get, :edit => :get, :update=>:put,:create=>:post } 
-    m.resources    :users,    :controller => 'member/users'
+    m.resources :users, :controller => 'member/users', :collection => { :index => :get, :edit => :get, :update=>[:put,:post],:create=>:post } 
     m.extras_boat  '/boats/:id/extras', :controller => 'member/boats', :action => 'extras'
     m.resources   :boats, :controller => 'member/boats'
     m.boat_checkout '/boats/:team_id/checkouts/:action/:id', :controller => 'member/checkouts'
