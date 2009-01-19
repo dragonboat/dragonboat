10e507584d18c831d82d81ab6c88c5268009e5fc
dina <dinchik-i@rambler.ru>
2009-01-19 14:45:16 +0200

    Upload changes on server

--- app/controllers/member/checkouts_controller.rb
+++ app/controllers/member/checkouts_controller.rb
@@ -5,7 +5,7 @@ before_filter :secure_site
 skip_before_filter :leave_secure_site
 
 include ActiveMerchant::Billing
-ActiveMerchant::Billing::Base.mode = :live #:test
+ActiveMerchant::Billing::Base.mode = :test #:live
 
  def index
     @order = Order.new
