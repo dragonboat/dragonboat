61f5e241d5e8b57ab8f28de023849ec29f64579a
dina <dinchik-i@rambler.ru>
2009-01-20 15:54:15 +0200

    Upload changes on server

--- lib/dragon_boat.rb
+++ lib/dragon_boat.rb
@@ -1,25 +1,25 @@
 module DragonBoat
   def self.included(base)
-    base.before_filter  :leave_secure_site, :www_check
+    base.before_filter  :www_check, :leave_secure_site
     base.send :helper_method
   end
   
 private
 #request.host
   def www_check
-    (redirect_to "http://www.#{APP_CONFIG['hostname']}#{request.request_uri}" and return false) unless request.subdomains.include?('www')
+    (redirect_to "http://www.#{request.host}#{request.request_uri}" and return false) unless request.subdomains.include?('www')
   end
   
   def leave_secure_site
     if APP_CONFIG['SSL']
-    (redirect_to "http://#{APP_CONFIG['hostname']}#{request.request_uri}" and return false) if request.ssl?
+    (redirect_to "http://#{request.host}#{request.request_uri}" and return false) if request.ssl?
     end
   end
 
   def secure_site
     if APP_CONFIG['SSL']
       unless request.ssl?
-        redirect_to "https://#{APP_CONFIG['hostname']}#{request.request_uri}"
+        redirect_to "https://#{request.host}#{request.request_uri}"
         return false
       end #unless RAILS_ENV == 'development'
     end
