295779056d5124237d57cbf0133c3e4809c57e3f
Cameron Hickey <cameronhickey@gmail.com>
2009-01-29 13:22:24 -0500

    calendar updates

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ActiveRecord::Base
   validates_presence_of :name, :start_date, :content
   
   def time
-    start_date.strftime("%I:%M%p") if start_date&&start_date.strftime("%H").to_i>0
+    start_date.strftime("%l:%M%p") if start_date&&start_date.strftime("%H").to_i>0
   end
 
 end
