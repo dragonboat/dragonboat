903308cc56897f71309f9264a5381694f00778df
dina <dinchik-i@rambler.ru>
2009-03-26 13:19:03 +0200

    Practice Schedule

--- app/controllers/practice_schedule_controller.rb
+++ app/controllers/practice_schedule_controller.rb
@@ -2,6 +2,7 @@ class PracticeScheduleController < ApplicationController
 
   def index
     # a chronological list of dates, times, and teams are displayed
+    # The field created_at contains the actual Date/Time practice
     @practices = Practice.find(:all, :order => "created_at")
   end
 
